da47d172af2b1ee625f9cee103b36c7862b28a27
Kliychok <118366164+Kliychok@users.noreply.github.com>
2023-03-10 00:24:50 +0300

    Module_18

---
+++ config.py
@@ -0,0 +1,9 @@
+TOKEN = "6234612615:AAH8YcjoxQhmlYdDJii8hcb6OyfQq7rTG5w"
+
+# TOKEN = '5729439291:AAHIEhTUBRG5cMLVgbs2siEUXZfxD3eAouU'
+
+keys = {
+    'евро': 'EUR',
+    'рубль': 'RUB',
+    'доллар': 'USD',
+}
---
+++ extensions.py
@@ -0,0 +1,34 @@
+import json
+import requests
+from config import keys
+
+class ConvertionException(Exception):
+    pass
+
+class CryptoConverter:
+    @staticmethod
+    def get_price(quote: str, base: str, amount: str):
+        if quote == base:
+            raise ConvertionException(f'Невозможно перевести одинаковые валюты {base}.')
+
+        try:
+            quote_ticker = keys[quote]
+        except KeyError:
+            raise ConvertionException(f'Не удалось обработать валюту {quote}')
+
+        try:
+            base_ticker = keys[base]
+        except KeyError:
+            raise ConvertionException(f'Не удалось обработать валюту {base}')
+
+        try:
+            amount = int(amount)
+        except ValueError:
+            raise ConvertionException(f'Не удалось обработать количество {amount}')
+
+        r = requests.get(f'https://min-api.cryptocompare.com/data/price?fsym={base_ticker}&tsyms={quote_ticker}')
+        # total_base = json.loads(r.content)[keys[base]]
+        total_base = float(json.loads(r.content)[keys[quote]])
+
+        # return total_basel_base
+        return total_base
